7cce4ceb886c2487c5f2e28d4aa62d608319547e
Gustavo-m72 <gustavomstefanello72@gmail.com>
2025-05-09 11:58:04 -0300

    Configuracao inicial do Taskpy, ruff e pytest

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
 from http import HTTPStatus
-from schemas import *
 
-app = FastAPI() 
+from fastapi import FastAPI
+
+from schemas import Message
+
+app = FastAPI()
+
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
-def read_root():    
+def read_root():
     return {'message': 'teste'}
-
